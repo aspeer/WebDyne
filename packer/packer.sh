6f196e26bc87aa81bcffaede390b1f854b8a8175
Andrew Speer <andrew.speer@isolutions.com.au>
2025-04-21 23:30:30 +0930

    Fix lib path in packer

--- packer/packer.sh
+++ packer/packer.sh
@@ -1,3 +1,5 @@
 cpanm --installdeps .
-pp -I ../pm-WebDyne -I ~/perl5/lib/perl5/ -M WebDyne -M Plack::Middleware::Lint -M Plack::Middleware::StackTrace -M Plack::Middleware::AccessLog \
+pp -I ../lib -I ~/perl5/lib/perl5/ -M WebDyne -M Plack::Middleware::Lint -M Plack::Middleware::StackTrace -M Plack::Middleware::AccessLog \
 -M Plack::Loader -M Plack::Handler::Standalone -o webdyne.psgi.pp ../bin/webdyne.psgi
+pp -p -I ../lib -I ~/perl5/lib/perl5/ -M WebDyne -M Plack::Middleware::Lint -M Plack::Middleware::StackTrace -M Plack::Middleware::AccessLog \
+-M Plack::Loader -M Plack::Handler::Standalone -o webdyne.par ../bin/webdyne.psgi
