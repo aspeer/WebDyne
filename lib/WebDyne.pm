990925fececc5f69f27c6fc2027685ac92493756
Andrew Speer <andrew.speer@isolutions.com.au>
2026-01-14 18:24:03 +1030

    Add pretty attribute option to JSON, update htmx tags to only render if hx-request request header present, update tests

--- lib/WebDyne.pm
+++ lib/WebDyne.pm
@@ -2529,7 +2529,8 @@ sub json {
     my $json_or=JSON->new() ||
         return err('unable to create new JSON object');
     debug("json_or: $json_or");
-    $json_or->canonical(defined($attr_hr->{'canonical'}) ? $attr_hr->{'canonical'} : WEBDYNE_JSON_CANONICAL);
+    $json_or->canonical(defined($attr_hr->{'canonical'}) ? ($attr_hr->{'canonical'} ? 1 : 0) : WEBDYNE_JSON_CANONICAL);
+    $json_or->pretty(defined($attr_hr->{'pretty'}) ? ($attr_hr->{'pretty'} ? 1 : 0)  : WEBDYNE_JSON_PRETTY);
     my $json=eval {$json_or->encode($json_xr)} ||
         return err('error %s on json_encode of %s', $@, Dumper($json_xr));
     debug("json %s", Dumper($json));
@@ -2542,8 +2543,9 @@ sub json {
     my %attr=(
         type => 'application/json',
         %{$attr_hr}
+        
     );
-    delete @attr{qw(perl package method handler)};
+    delete @attr{qw(perl package method handler pretty canonical)};
 
 
     #  Render and return
@@ -2575,12 +2577,26 @@ sub htmx {
     debug("hx_request header: $hx_request");
     
     
+    #  If not HTMX request don't display unless forced
+    #
+    unless($hx_request || $attr_hr->{'force'} || WEBDYNE_HTMX_FORCE) {
+    
+
+        #  Not HTMX request, bail
+        #
+        debug('not htmx request and no force attribute or flag, bailing');
+        return \undef;
+        
+    }
+    
+    
     #  Check if we want to fire on match
     #
     if (exists ($attr_hr->{'display'})) {
         unless ($attr_hr->{'display'}) {
             #  Defined but no match. Bail
             #
+            debug('display attribute present but no match, bailing');
             return \undef;
         }
     }
