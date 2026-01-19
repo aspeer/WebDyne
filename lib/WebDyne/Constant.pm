d416585eeaeb28a47f4ad6880b042155a7b43952
Andrew Speer <andrew.speer@isolutions.com.au>
2026-01-19 23:16:54 +1030

    VERSION increment: 2.073

--- lib/WebDyne/Constant.pm
+++ lib/WebDyne/Constant.pm
@@ -32,7 +32,7 @@ require Opcode;
 
 #  Version information
 #
-$VERSION='2.072';
+$VERSION='2.073';
 
 
 #  Get mod_perl version taking intio account legacy strings. Clear $@ after evals
--- lib/WebDyne/Util.pm
+++ lib/WebDyne/Util.pm
@@ -42,7 +42,7 @@ require Exporter;
 
 #  Version information
 #
-$VERSION='2.072';
+$VERSION='2.073';
 
 
 #  Var to hold package wide hash, for data shared across package, and error stack
