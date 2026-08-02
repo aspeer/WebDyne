6c2362bc40af95c0aaaf156777e4bafbffb26191
Andrew Speer <andrew.speer@isolutions.com.au>
2026-08-02 20:51:53 +0930

    Update doc to include link to PAGI examples, update docker/cpanfile to install additional modules installed by WebDyne

--- doc/mkdocs/hooks/local_deploy.py
+++ doc/mkdocs/hooks/local_deploy.py
@@ -2,7 +2,7 @@ import re
 
 def rewrite_md(markdown, page, config, files):
     markdown = re.sub(r'\(https?://demo.webdyne.org/example(/[^)]*)\)',
-                      r'(http://192.168.0.1:5000\1)', markdown)
+                      r'(http://127.0.0.1:5000\1)', markdown)
     base = (config.get("site_url") or "").rstrip("/")
     markdown = markdown.replace("{{CDN}}", f"{base}/assets")
     return markdown
