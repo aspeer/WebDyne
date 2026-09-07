679582cc3bfbfa04120522f60c67424cf949f120
Andrew Speer <andrew.speer@isolutions.com.au>
2026-09-08 00:07:59 +0930

    VERSION increment: 3.028

--- lib/WebDyne/PAGI.pm
+++ lib/WebDyne/PAGI.pm
@@ -64,7 +64,7 @@ my %ENV_BASE=(
 
 #  Version information
 #
-$VERSION='3.027';
+$VERSION='3.028';
 
 
 #==================================================================================================
@@ -888,7 +888,7 @@ The module relies on `WebDyne::Request::PAGI` for normalized request handling an
 
 Andrew Speer <andrew.speer@isolutions.com.au>
 
-# LICENSE and COPYRIGHT #
+# LICENSE and COPYRIGHT
 
 This file is part of WebDyne.
 
@@ -900,6 +900,7 @@ the same terms as the Perl 5 programming language system itself.
 Full license text is available at:
 
 <http://dev.perl.org/licenses/>
+
 
 =end markdown
 
